3ff3f7f80c9d0a651ad4c7c51e16e00da50e11bb
Christoph Heer <Christoph.Heer@gmail.com>
2012-07-31 16:36:01 +0100

    Change title

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-SITE_NAME = "Christoph Heer"
+SITE_NAME = "Christoph Heer's thoughts and notes"
 AUTHOR_NAME = "Christoph Heer"
 AUTHOR_EMAIL = "christoph.heer@googlemail.com"
 
